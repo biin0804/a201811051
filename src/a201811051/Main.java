6f67c609cee19e49a88d53884a6ca4d307661262
Kim, Junghun <kebin10804@gmail.com>
2024-11-20 10:47:39 +0900

    four

--- src/a201811051/Main.java
+++ src/a201811051/Main.java
@@ -4,5 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		// 자신의 학번과 이름을 출력하라
 		System.out.println("201514099 홍길동");
+		System.out.println(World.factorial(3));
+		System.out.println(World.factorial(6));
+		System.out.println(World.factorial(9));
 	}
 }
